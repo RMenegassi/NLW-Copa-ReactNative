e8fa6e65ec912c3ca2d3c325517796fc0fd0b5e9
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-11-03 23:56:26 -0300

    update: Set Day

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {CardContainer} from './styles';
+import moment from 'moment';
 
 import HeaderDate from './HeaderDate';
 import MatchesList from './MatchesList';
 
 const Card = () => {
+  const [day, setDay] = useState(
+    moment('20112022', 'DDMMYYYY') - moment() > 0
+      ? '20/11'
+      : moment('03122022', 'DDMMYYYY') - moment() < 0
+      ? '02/12'
+      : moment().format('DD/MM'),
+  );
+
+  const addDay = () => {
+    const forwardDay = moment(`${day}`, 'DD/MM').add(1, 'days').format('DD/MM');
+    setDay(forwardDay);
+  };
+
+  const subDay = () => {
+    const backDay = moment(`${day}`, 'DD/MM')
+      .subtract(1, 'days')
+      .format('DD/MM');
+    setDay(backDay);
+  };
+
   return (
     <CardContainer style={{borderBottomColor: '#047c3f', borderBottomWidth: 2}}>
-      <HeaderDate />
-      <MatchesList />
+      <HeaderDate day={day} addDay={addDay} subDay={subDay} />
+      <MatchesList day={day} />
     </CardContainer>
   );
 };
