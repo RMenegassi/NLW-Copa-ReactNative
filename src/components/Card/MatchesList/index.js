ea83ad8bd9687a67c2d78f087e620647017a9027
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-11-03 21:27:24 -0300

    feat: Create MatchesList

---
+++ src/components/Card/MatchesList/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {View, Text, Image, ListMatches, PartidaView} from './styles';
+
+import {images} from '../../../images';
+import {matches} from '../../../assets/matches';
+
+const MatchesList = () => {
+  const dataFiltrada = matches.filter(item => {
+    return item.dia === '02/12';
+  });
+
+  return (
+    <View>
+      <ListMatches
+        data={dataFiltrada[0].jogos}
+        renderItem={({item, index}) => (
+          <PartidaView
+            style={
+              index != 0
+                ? {borderTopColor: '#323238', borderTopWidth: 1, paddingTop: 24}
+                : ''
+            }>
+            <Image source={images[item.mandante]} />
+            <Text>{item.horario}</Text>
+            <Image source={images[item.visitante]} />
+          </PartidaView>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MatchesList;
---
+++ src/components/Card/MatchesList/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const View = styled.View``;
+
+export const ListMatches = styled.FlatList`
+  flex-grow: 0;
+`;
+
+export const PartidaView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+  border-top: 1px solid #323238;
+  margin-bottom: 24px;
+`;
+
+export const Text = styled.Text`
+  color: #e1e1e6;
+  font-size: 36px;
+  font-weight: 900;
+`;
+
+export const Image = styled.Image`
+  width: 50px;
+  height: 50px;
+`;
