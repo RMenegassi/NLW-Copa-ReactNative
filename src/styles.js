f0f130c0306ae8922aaa0e9d88d12db2cff94fb7
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-11-03 21:26:50 -0300

    feat: Create Style Component

---
+++ src/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  background-color: transparent;
+  margin-top: 30px;
+`;
+
+export const CardView = styled.View``;
