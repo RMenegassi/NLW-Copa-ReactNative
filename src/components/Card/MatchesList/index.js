b621733b4a870ae742633dc17a502c2a4d0e6ff1
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-11-21 23:11:08 -0300

    feat: add DB to MatrchesList

--- src/components/Card/MatchesList/index.js
+++ src/components/Card/MatchesList/index.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-import {View, Text, Image, ListMatches, PartidaView} from './styles';
+import {View, Text, Image, ListMatches, PartidaView, Gols} from './styles';
 
+import {getMatches} from '../../../services/Matches';
 import {images} from '../../../images';
-import {matches} from '../../../assets/matches';
 
 const MatchesList = ({day}) => {
-  const dataFiltrada = matches.filter(item => {
-    return item.dia === day;
-  });
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const loadMatches = async () => {
+      const matchesDay = await getMatches(day);
+      setData(matchesDay);
+    };
+    loadMatches();
+  }, [day]);
 
   return (
     <View>
-      <ListMatches
-        data={dataFiltrada[0].jogos}
-        renderItem={({item, index}) => (
-          <PartidaView
-            style={
-              index != 0
-                ? {borderTopColor: '#323238', borderTopWidth: 1, paddingTop: 24}
-                : ''
-            }>
-            <Image source={images[item.mandante]} />
-            <Text>{item.horario}</Text>
-            <Image source={images[item.visitante]} />
-          </PartidaView>
-        )}
-      />
+      {data !== undefined && (
+        <ListMatches
+          data={data[0].jogos}
+          renderItem={({item, index}) => (
+            <PartidaView
+              style={
+                index != 0
+                  ? {
+                      borderTopColor: '#323238',
+                      borderTopWidth: 1,
+                      paddingTop: 24,
+                    }
+                  : ''
+              }>
+              <Image source={images[item.mandante]} />
+              <Gols
+                keyboardType="number-pad"
+                maxLength={2}
+                placeholder={item.golsMandante}
+              />
+              <Text>{item.horario}</Text>
+              <Gols
+                keyboardType="number-pad"
+                maxLength={2}
+                defaultValue={item.golsVisitante}
+              />
+              <Image source={images[item.visitante]} />
+            </PartidaView>
+          )}
+        />
+      )}
     </View>
   );
 };
